b5fa885cce99760f05f916e2865cf31f3c2e8528
Hama Pashazadeh <pashazad@msu.edu>
2025-05-10 14:43:49 -0400

    Add a summary of learning so far to hello_cargo

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, cargo!");
 }
+
+//
+// Learned so far:
+//
+
+// Can create new projects using `cargo new`
+// Can build (compile) using `cargo build`
+// Can build and run using`cargo run`
+// Can compile (without creating the binary) using `cargo check`
